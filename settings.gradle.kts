a62331b4227efe5f171c19f913af647666b8a24e
windljasem <73621123+windljasem@users.noreply.github.com>
2026-01-01 00:02:24 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "ugcc-calendar"
 include(":app")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "ugcc-calendar"
+include(":app")
