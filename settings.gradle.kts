6cee7bdb71b478117494a6b54991bbea9b3c3dd3
windljasem <73621123+windljasem@users.noreply.github.com>
2026-01-03 23:31:24 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,15 +16,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "UGCC-Calendar"
 include(":app")
-```
-
-5. Внизу натисни **"Commit changes..."** → **"Commit changes"**
-
----
-
-## Крок 2: Створюємо файл `build.gradle.kts` (кореневий)
-
-1. **"Add file"** → **"Create new file"**
-2. Назва:
-```
-   build.gradle.kts
