b670111ae30d64bd7a927b79d10e9eb0e02da2dc
windljasem <73621123+windljasem@users.noreply.github.com>
2026-01-03 23:18:56 +0200

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+org.gradle.jvmargs=-Xmx2048m -Dfile.encoding=UTF-8
+android.useAndroidX=true
+kotlin.code.style=official
+android.nonTransitiveRClass=true
+```
+
+4. **Commit changes**
+
+---
+
+## Крок 4: Створюємо `app/build.gradle.kts`
+
+1. **"Add file"** → **"Create new file"**
+2. Назва (важливо з папкою!):
+```
+   app/build.gradle.kts
